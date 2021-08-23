65bad3efddba3e66425417f1f3d16057eb0b270b
sylwia-luczak <88457687+sylwia-luczak@users.noreply.github.com>
2021-08-23 14:31:16 +0200

    Update GothicLLamasBEcreateDB.sql
    Add NOT NULL to columns with varchar data

--- DataBaseScripts/GothicLLamasBEcreateDB.sql
+++ DataBaseScripts/GothicLLamasBEcreateDB.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS capabilityAndRoles (
     'People', 'Commercial and Financial Management', 'Business Services Support'),
     bandLevel ENUM ('7 - apprentice', '6 - trainee',  '5 - associate', '4 - senior associate',
     '3 - consultant', '2 - manager', '1 - principal '),
-    jobRole VARCHAR(50),
-    jobDescription VARCHAR(500),
-    linkToFullDescription VARCHAR (200)
+    jobRole VARCHAR(50) NOT NULL,
+    jobDescription VARCHAR(500) NOT NULL,
+    linkToFullDescription VARCHAR (200) NOT NULL
 );

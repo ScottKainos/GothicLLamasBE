f9956b8d368c6175246bda6b2ecc849553928dec
ciaranmullan <ciaran.mullan@kainos.com>
2021-09-02 11:37:24 +0100

    Setting up mappers for each use case

--- GothicLlamasAPI/src/main/java/com/kainos/ea/objects/JobRoleMapper.java
+++ GothicLlamasAPI/src/main/java/com/kainos/ea/objects/JobRoleMapper.java
@@ -5,13 +5,12 @@ import org.jdbi.v3.core.statement.StatementContext;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class JobRolesAndSpecificationMapper implements RowMapper<CapabilitiesAndRoles> {
+public class JobRoleMapper implements RowMapper<CapabilitiesAndRoles> {
     @Override
     public CapabilitiesAndRoles map(ResultSet r, StatementContext ctx) throws SQLException {
         CapabilitiesAndRoles cap = new CapabilitiesAndRoles();
         cap.setJobRole(r.getString("jobRole"));
-        cap.setJobDescription(r.getString("jobDescription"));
-        cap.setLinkToFullDescription(r.getString("linkToFullDescription"));
         return cap;
     }
 }
+
--- GothicLlamasAPI/src/main/java/com/kainos/ea/resources/CapabilitiesAndRolesDAO.java
+++ GothicLlamasAPI/src/main/java/com/kainos/ea/resources/CapabilitiesAndRolesDAO.java
@@ -1,24 +1,18 @@
 package com.kainos.ea.resources;
 
-import com.kainos.ea.DBConnection;
-import com.kainos.ea.objects.CapabilitiesAndRoles;
-import com.kainos.ea.objects.JobRoleAndCapabilityMapper;
-import com.kainos.ea.objects.JobRoleMapper;
-import com.kainos.ea.objects.JobRolesAndSpecificationMapper;
+import com.kainos.ea.objects.*;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface CapabilitiesAndRolesDAO {
 
     @SqlQuery("SELECT DISTINCT jobRole FROM capabilityAndRoles;")
-    @RegisterRowMapper(JobRolesAndSpecificationMapper.class)
+    @RegisterRowMapper(JobRoleMapper.class)
     List<CapabilitiesAndRoles> getAllJobRoles();
 
 
@@ -27,7 +21,7 @@ public interface CapabilitiesAndRolesDAO {
     List<CapabilitiesAndRoles> getJobRoleAndSpecifications();
 
     @SqlQuery("SELECT jobRole, capability FROM capabilityAndRoles;")
-    @RegisterRowMapper(JobRolesAndSpecificationMapper.class)
+    @RegisterRowMapper(JobRoleAndCapabilityMapper.class)
     List<CapabilitiesAndRoles> getJobRoleAndCapability();
 
 }
